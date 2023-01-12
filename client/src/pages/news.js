6a033091829f3f246cef2281b5cd74dc2f57e08d
islandskan <sigridureggertsdottir.art@gmail.com>
2023-01-12 20:54:25 +0100

    add markup for News page

--- client/src/pages/news.js
+++ client/src/pages/news.js
@@ -1,11 +1,22 @@
-function News() {
+import { MetaData } from '../components/MetaData';
+import styles from '../styles/News.module.css';
+// import { NewsCard } from '../components/NewsCard';
+// import { useEffect, useState } from 'react';
+
+// refactor to getStaticProps
+const News = () => {
+    // const newsElements = newsCards.map((card) => (
+    //     <NewsCard key={card.sys.id} card={card} />
+    // ));
+
     return (
         <>
-            <div>
-                <h2>News</h2>
+            <MetaData page='Calender' />
+            <div className={`container ${styles.news}`}>
+                <div className={styles.newsList}></div>
             </div>
         </>
     );
-}
+};
 
 export default News;
