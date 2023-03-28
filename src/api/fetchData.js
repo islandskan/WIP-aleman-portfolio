ee1d40dec7b383a1914effbe9c38ccedd3a52eb1
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:00:47 +0200

    move client and data

--- src/api/fetchData.js
+++
@@ -1,8 +0,0 @@
-import { client } from './client.js';
-
-export const getNews = async () => {
-    console.log(client);
-    const news = await client.getEntries({ content_type: 'newsCards' });
-    console.log(news);
-    return news.items;
-};
