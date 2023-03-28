1b962600d31cf660b7fc662e288ca294cb23f5e9
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:20:04 +0200

    import skip to main link and check if the current page is the landing page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,26 @@
-import { Footer } from './Footer';
 import { Navbar } from './Navbar';
+import SkipToMain from './SkipToMain';
+import styles from '../styles/Landing.module.css';
+import { useRouter } from 'next/router';
 
 export const Layout = ({ children }) => {
+    const router = useRouter();
+    const currentPage = router.pathname;
+    const isLandingPage = currentPage === '/';
+
+    console.log(styles);
     return (
-        <div className='layout'>
-            <Navbar />
-            {children}
-            <Footer />
-        </div>
+        <>
+            <div
+                className={`layout ${
+                    isLandingPage ? styles.landingLayout : ''
+                }`}
+            >
+                <SkipToMain href='#main-content' />
+                <Navbar className={`${isLandingPage ? 'landingPage' : ''}`} />
+
+                <main tabIndex={-1}>{children}</main>
+            </div>
+        </>
     );
 };
