bdd50fc216fd0a02340c913982bd65460db54164
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:24:27 +0200

    setup PDF element

---
+++ src/components/PDFElement.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/components/PDFElement.module.css';
+
+export const PDFElement = ({ hilmaPDF }) => {
+    const PDFimg = `https:${hilmaPDF.thumbnail.fields.file.url}`;
+    const PDFurl = `https:${hilmaPDF.pdfAsset.fields.file.url}`;
+    const { height: HEIGHT, width: WIDTH } =
+        hilmaPDF.thumbnail.fields.file.details.image;
+
+    return (
+        <figure className={styles.PDFthumbnail}>
+            <Link href={PDFurl} className={styles.PDF__link}>
+                <Image
+                    src={PDFimg}
+                    alt={hilmaPDF.thumbnail.fields.title}
+                    width={WIDTH / 1.5}
+                    height={HEIGHT / 1.5}
+                    className={styles.PDFthumbnail__img}
+                />
+            </Link>
+            <figcaption className={styles.PDFthumbnail__txt}>
+                {hilmaPDF.pdfText}
+            </figcaption>
+        </figure>
+    );
+};
