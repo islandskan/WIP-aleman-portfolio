d0ceae65118415488ec832bdf8c144c4dd3e788b
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:22:36 +0200

    check if current page is the link in nav and function to close the mobile nav bar when you click outside

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,61 @@
 import Link from 'next/link';
 import { BurgerMenu } from './BurgerMenu';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
 
-export const Navbar = () => {
+export const Navbar = ({ className }) => {
     const [isExpanded, setIsExpanded] = useState(false);
+    let menuRef = useRef();
+    const router = useRouter();
+    const currentPage = router.pathname;
 
+    console.log(className);
     let isMenuOpen = isExpanded ? 'open' : '';
+
+    // closes mobile menu when you click somewhere outside
+    useEffect(() => {
+        const handler = (e) => {
+            if (!menuRef.current.contains(e.target)) {
+                setIsExpanded(false);
+            }
+        };
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler);
+        };
+    }, []);
 
     const handleClick = () => {
         setIsExpanded((prevState) => !prevState);
     };
-    // logic to close navbar when you click outside
+
+    const links = [
+        { url: '/', name: 'Home' },
+        { url: '/works', name: 'Works' },
+        { url: '/about', name: 'About' },
+        { url: '/news', name: 'News' },
+        { url: '/contact', name: 'Contact' },
+    ];
+    const menuLinks = links.map((link, index) => (
+        <li
+            key={index}
+            className={`menu__link ${currentPage === link.url ? 'active' : ''}`}
+        >
+            <Link href={link.url}>{link.name}</Link>
+        </li>
+    ));
 
     return (
-        <nav>
-            <div className='logo'>
-                <h1>Madeleine Aleman</h1>
-            </div>
-            <BurgerMenu handleClick={handleClick} isExpanded={isExpanded} />
-            <div className={`menu ${isMenuOpen}`}>
-                <ul>
-                    <li>
-                        <Link href='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link href='/news'>News</Link>
-                    </li>
-                    <li>
-                        <Link href='/contact'>Contact</Link>
-                    </li>
-                </ul>
+        <nav ref={menuRef} className={className}>
+            <div className='nav_container'>
+                <Link className='logo' href='/' aria-hidden='true'>
+                    Madeleine Aleman
+                </Link>
+                <BurgerMenu handleClick={handleClick} isExpanded={isExpanded} />
+                <div className={`menu ${isMenuOpen}`}>
+                    <ul>{menuLinks}</ul>
+                </div>
             </div>
         </nav>
     );
