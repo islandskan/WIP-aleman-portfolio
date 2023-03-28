4604c61b4d798059cc1183f9055b93a46e7a079d
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:43:29 +0200

    setup function to save filled out inputs to the local storage

---
+++ src/utils/useStorage.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+/*
+useEffect: is activated by default after the very first render and then each time the state changed. As the name suggests, it is used to perform an effect each time the state is changed.
+Good for:
+- Config event listeners
+- retrieving data from an API
+- deleting event listeners before the component(that the event listeners were part of) is removed from the DOM
+
+localStorage = access to a browser's storage object
+this object contains
+*/
+
+// useState
+// const [storage, setStorage] = useState([]);
+
+// useEffect
