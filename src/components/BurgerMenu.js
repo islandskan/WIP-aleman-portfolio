bd1f964e5a696462d5ac4e8a33b0a89682585943
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:12:55 +0200

    add SR

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,14 +1,17 @@
-export const BurgerMenu = (props) => {
-    let burgerStyles = props.isExpanded ? 'open' : '';
+import { SrOnly } from './SrOnly';
+export const BurgerMenu = ({ isExpanded, handleClick }) => {
+    let burgerStyles = isExpanded ? 'open' : '';
+    let burgerSrText = isExpanded ? 'Close Mobile Menu' : 'Open Mobile Menu';
     return (
         <>
             <button
                 className={`burger-menu ${burgerStyles}`}
-                onClick={props.handleClick}
+                onClick={handleClick}
             >
-                <div className='line'></div>
-                <div className='line'></div>
-                <div className='line'></div>
+                <div className='line' aria-hidden='true'></div>
+                <div className='line' aria-hidden='true'></div>
+                <div className='line' aria-hidden='true'></div>
+                <SrOnly text={burgerSrText} />
             </button>
         </>
     );
