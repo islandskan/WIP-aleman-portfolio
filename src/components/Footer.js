e0162b71a8113303a77c341093f8b654db17e3b9
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:15:11 +0200

    remove links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,11 @@
-import Link from 'next/link';
-
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer className='footer'>
-            <div className='footer-list'>
-                <h6 className='footer-heading'>Other Links</h6>
-                <ul className='footer-links'>
-                    <li>
-                        <Link
-                            href='https://www.instagram.com/madeleinealeman/'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            Instagram
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href='https://ed-art.se/sv/konstnarer/132__madeleine-aleman/'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            Buy Prints (Ed Art)
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <small>&copy; Madeleine Aleman {currentYear}</small>
+            <section className='footer__link-container'></section>
+            <section className='footer__small-container'>
+                <small>&copy; Madeleine Aleman 2006 - {currentYear}</small>
+            </section>
         </footer>
     );
 };
