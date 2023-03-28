b652457ad48eacdb10b7c3b4ee9b6a847dcaa681
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:18:25 +0200

    set up image element for archive page

---
+++ src/components/ImageElements.js
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import styles from '../styles/Project.module.css';
+export const ImageElement = ({ image }) => {
+    const imageUrl = `https:${image.fields.file.url}`;
+    const imageAlt = image.fields.description;
+    return (
+        <figure className={styles.archiveImgFigure}>
+            <Image
+                className={styles.archiveImg}
+                src={imageUrl}
+                alt={imageAlt}
+                height={350}
+                width={300}
+            />
+        </figure>
+    );
+};
