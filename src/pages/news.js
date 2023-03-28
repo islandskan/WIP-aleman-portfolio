ca4fd2235bb8df83bf5d1e1b28695f759edbc633
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:33:50 +0200

    fetch the news data

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,42 +1,54 @@
 import { MetaData } from '../components/MetaData';
 import styles from '../styles/News.module.css';
 import { NewsCard } from '../components/NewsCard';
-// import { useEffect, useState } from 'react';
-// import { fetchContent } from '../api/contentful';
+import { createClient } from 'contentful';
+// import { fetchEntries } from '../api/fetchEntries';
+
+export async function getStaticProps() {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+    const res = await client.getEntries({ content_type: 'newsCards' });
+
+    return {
+        props: {
+            newsCards: res.items,
+        },
+    };
+}
 
 // export async function getStaticProps() {
-//     // the point of getStaticProps is to get data from the headless CMS at build time
-//     const { newsCardCollection } = await fetchContent(`
-//     query {
-//         newsCardCollection {
-//           items {
-//                 startDate
-//                 endDate
-//                 calenderUrl
-//                 calenderUrlName
-//             }
-//           }
-//         }
-//       }
-//     `);
-
+//     const res = await fetchEntries('newsCards');
+//     const newsCards = res.map((item) => {
+//         return item.fields;
+//     });
 //     return {
 //         props: {
-//             newsCard: newsCardCollection.items,
+//             newsCards,
 //         },
 //     };
 // }
-const News = () => {
-    // console.log(newsCard);
-    // const newsElements = newsCards.map((card) => (
-    //     <NewsCard key={card.sys.id} card={card} />
-    // ));
+const News = ({ newsCards }) => {
+    // const[cardOpen, setCardOpen] = useState(false)
+    // const handleToggle = (index) => {
+    //     if (clicked === index) {
+    //      return setClicked("0");
+    //     }
+    //     setClicked(index);
+    // }
+
+    const newsElements = newsCards.map((card, index) => (
+        <>
+            <NewsCard key={card.sys.id} card={card} btnId={index} />
+        </>
+    ));
 
     return (
         <>
-            <MetaData page='Contact' />
-            <div className={`container ${styles.news}`}>
-                <div className={styles.newsList}></div>
+            <MetaData page='News' />
+            <div id='news' className={`container ${styles.news}`}>
+                <div className={styles.newsList}>{newsElements}</div>
             </div>
         </>
     );
