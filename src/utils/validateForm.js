74d71ee04950b5f99a937c6a64c9e1cb01918849
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:43:52 +0200

    setup function to validate the input values

---
+++ src/utils/validateForm.js
@@ -0,0 +1,22 @@
+//  // Helper functions.
+//  function isValidEmail(e) {
+//     regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     var email = e.value;
+//     if (!regEx.test(email)) {
+//         return false;
+//     }
+// }
+
+// function isValidChar(e) {
+//     regEx = /^[a-zA-Z@#$%!?^&*()_+\-=\[\]{};':"\\|,.\/? ]*$/;
+//     var value = e.value;
+//     if (!regEx.test(value)) {
+//         return false;
+//     }
+// }
+
+// function isValidPhone(e) {
+//   regEx = /^[+]?[(]?[+]?\d{2,4}[)]?[-\s]?\d{2,8}[-\s]?\d{2,8}$/;
+//   var value = e.value;
+//   if(!regEx.test(value)) {
+//     return false;
