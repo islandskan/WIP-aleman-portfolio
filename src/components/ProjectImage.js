d2ffc8bb059d04ac63a88eb6733fd2530d40026c
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:27:24 +0200

    set up project image element

---
+++ src/components/ProjectImage.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import styles from '../styles/Project.module.css';
+export const ProjectImage = ({ image, ariaSetSize, index }) => {
+    const imageUrl = `https:${image.image.fields.file.url}`;
+    const imageInfo = [image.imageTitle, image.materialOrMethod, image.size];
+    return (
+        <figure
+            className={styles.projectImageFigure}
+            aria-setsize={ariaSetSize}
+            aria-posinset={index}
+        >
+            <Image
+                className={styles.projectImage}
+                src={imageUrl}
+                alt={image.imageAltText}
+                height={900}
+                width={800}
+            />
+            <figcaption>{imageInfo.join(' - ')}</figcaption>
+        </figure>
+    );
+};
