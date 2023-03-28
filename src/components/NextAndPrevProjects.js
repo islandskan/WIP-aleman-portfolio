72aeba31142ba0533dff4e2abd1aa7c864eacfd3
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:23:59 +0200

    set up next and prev element

---
+++ src/components/NextAndPrevProjects.js
@@ -0,0 +1,24 @@
+import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const NextAndPrevProjects = () => {
+    const router = useRouter();
+    console.log(router);
+    return (
+        <div className='nextprevProjects'>
+            <Link href='#' className='nextPrevLink prev'>
+                <GrLinkPrevious className='icon' />
+                <span>Prev</span>
+            </Link>
+
+            <Link href='#' className='nextPrevLink next'>
+                <span>Next</span>
+                <GrLinkNext className='icon' />
+            </Link>
+        </div>
+    );
+};
+
+export default NextAndPrevProjects;
