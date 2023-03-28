bb09ea92c8df0b87ae283636dbc5797cc698564b
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-28 13:13:59 +0200

    add onClick handler as props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 // props for click handlers as well
 
-export const Button = ({ type, classname, text }) => {
+export const Button = ({ type, classname, text, onClick }) => {
     return (
-        <button type={type} className={`btn ${classname}`}>
+        <button type={type} className={`btn ${classname}`} onClick={onClick}>
             {text}
         </button>
     );
