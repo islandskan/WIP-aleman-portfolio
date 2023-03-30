82e9cdea8e35de6329e1bb745c1f69652a337124
islandskan <sigridureggertsdottir.art@gmail.com>
2023-03-30 09:55:23 +0200

    remove useRouter

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,12 @@
 import { Navbar } from './Navbar';
 import SkipToMain from './SkipToMain';
-import styles from '../styles/Landing.module.css';
-import { useRouter } from 'next/router';
 
 export const Layout = ({ children }) => {
-    const router = useRouter();
-    const currentPage = router.pathname;
-    const isLandingPage = currentPage === '/';
-
-    console.log(styles);
     return (
         <>
-            <div
-                className={`layout ${
-                    isLandingPage ? styles.landingLayout : ''
-                }`}
-            >
+            <div className='layout'>
                 <SkipToMain href='#main-content' />
-                <Navbar className={`${isLandingPage ? 'landingPage' : ''}`} />
-
+                <Navbar />
                 <main tabIndex={-1}>{children}</main>
             </div>
         </>
