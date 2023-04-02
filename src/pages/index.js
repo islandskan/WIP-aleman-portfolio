7357e76679abfefc659a82d8a415078fc15772f6
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-02 17:18:29 +0200

    remove landing page and replace with works/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
+import styles from '../styles/Home.module.css';
+import ProjectCard from '../components/ProjectCard';
 import { MetaData } from '../components/MetaData';
-import styles from '../styles/Landing.module.css';
-function HomePage() {
+// import { EmailJSForm } from '../components/test-components/EmailJSForm';
+import { createClient } from 'contentful';
+// import isInternal from '../utils/isInternal';
+import { renderProjects } from '../utils/sortProjects.js';
+
+export async function getStaticProps() {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+    const res = await client.getEntries({ content_type: 'projectCard' });
+    if (!res) {
+        return { notFound: true };
+    }
+
+    return {
+        props: {
+            projectCards: res.items,
+        },
+    };
+}
+
+function Works({ projectCards }) {
+    const projectList = renderProjects(ProjectCard, projectCards);
+
     return (
         <>
-            <MetaData page='Home' />
-            <div className={styles.backgroundImg}></div>
+            <MetaData page='Start' />
+            <ul id='works' className={`container ${styles.projectsList}`}>
+                {projectList}
+            </ul>
         </>
     );
 }
 
-export default HomePage;
+export default Works;
