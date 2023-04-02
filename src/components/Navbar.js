e6f91508ba95b6cb2b2d62a98496280537c06a2e
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-02 17:19:59 +0200

    remove link to old landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,6 @@ export const Navbar = () => {
 
     const links = [
         { url: '/', name: 'Home' },
-        { url: '/works', name: 'Works' },
         { url: '/about', name: 'About' },
         { url: '/news', name: 'News' },
         { url: '/contact', name: 'Contact' },
