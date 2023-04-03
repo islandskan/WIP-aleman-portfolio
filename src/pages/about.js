57b7dd1e9743145f4b99578f7db4d425d395bb65
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-03 08:44:47 +0200

    fix envs

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,8 +10,8 @@ import { INLINES } from '@contentful/rich-text-types';
 
 export async function getStaticProps() {
     const client = createClient({
-        space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     });
     const res = await client.getEntries({ content_type: 'about' });
     if (!res) {
