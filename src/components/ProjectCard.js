7cc773d72fb1ca25e8bdc023688e76c5ef5be3b5
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-05 12:51:10 +0200

    remove duplicate of classname .projectCard__thumbnail

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,27 +4,30 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import { SrOnly } from './SrOnly.js';
 import { resizeImg } from '../utils/resizeProjectImg';
+
 const ProjectCard = ({ card }) => {
-    const cardImgSizes = resizeImg(card);
+    // const cardImgSizes = resizeImg(card);
+
+    console.log(card.fields.projectThumbnail.fields.file.details.image);
 
     const { slug, projectThumbnail, projectTitle } = card.fields;
-    const { height, width } = cardImgSizes;
-    console.log(`${projectTitle}, Height: ${height}, width: ${width}`);
+    // const { height, width } = cardImgSizes;
+    // console.log(`${projectTitle}, Height: ${height}, width: ${width}`);
     const thumbnailImgUrl = `https:${projectThumbnail.fields.file.url}`;
+    const { height, width } =
+        card.fields.projectThumbnail.fields.file.details.image;
+    console.log(height, width);
 
     return (
         <li className={`${styles.projectCard}`} key={slug}>
-            <Link
-                href={`/works/${slug}`}
-                className={styles.projectsCard__thumbnail}
-            >
+            <Link href={`/works/${slug}`}>
                 <SrOnly text={`Go to ${projectTitle}`} />
                 <h4 className={styles.projectCard__title}>{projectTitle}</h4>
                 <Image
                     src={thumbnailImgUrl}
                     alt={projectTitle}
-                    height={height}
-                    width={width}
+                    height='500'
+                    width='500'
                     className={styles.projectsCard__thumbnail}
                     loading='lazy'
                 />
