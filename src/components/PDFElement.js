f05a8a68ead7ca921e57f351e092bf49f5c246ba
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-10 20:42:38 +0200

    add component for the other link thumbnail

--- src/components/PDFElement.js
+++ src/components/PDFElement.js
@@ -2,18 +2,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/components/PDFElement.module.css';
 
-export const PDFElement = ({ hilmaPDF }) => {
-    const PDFimg = `https:${hilmaPDF.thumbnail.fields.file.url}`;
-    const PDFurl = `https:${hilmaPDF.pdfAsset.fields.file.url}`;
+export const PDFElement = ({ item }) => {
+    const PDFimg = `https:${item.thumbnail.fields.file.url}`;
+    const PDFurl = `https:${item.pdfAsset.fields.file.url}`;
+
     const { height: HEIGHT, width: WIDTH } =
-        hilmaPDF.thumbnail.fields.file.details.image;
+        item.thumbnail.fields.file.details.image;
 
     return (
         <figure className={styles.PDFthumbnail}>
             <Link href={PDFurl} className={styles.PDF__link}>
                 <Image
                     src={PDFimg}
-                    alt={hilmaPDF.thumbnail.fields.title}
+                    alt={item.thumbnail.fields.title}
                     width={WIDTH / 1.5}
                     height={HEIGHT / 1.5}
                     loading='lazy'
@@ -21,8 +22,31 @@ export const PDFElement = ({ hilmaPDF }) => {
                 />
             </Link>
             <figcaption className={styles.PDFthumbnail__txt}>
-                {hilmaPDF.pdfText}
+                {item.pdfText}
             </figcaption>
+        </figure>
+    );
+};
+
+export const BlogImg = ({ item }) => {
+    console.log(item);
+
+    return (
+        <figure className={styles.PDFthumbnail}>
+            <Link
+                href={item.thumbnailLink}
+                className={styles.PDF__link}
+                target='_blank'
+            >
+                <Image
+                    src={`https:${item.thumbnail.fields.file.url}`}
+                    alt={item.thumbnail.fields.title}
+                    width={item.thumbnail.fields.file.details.image.width}
+                    height={item.thumbnail.fields.file.details.image.height}
+                    className={styles.PDFthumbnail__img}
+                />
+            </Link>
+            <figcaption className={styles.PDFthumbnail__txt}></figcaption>
         </figure>
     );
 };
