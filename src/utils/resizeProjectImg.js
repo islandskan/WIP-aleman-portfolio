5ba57dc1572aa0d4d8e2a77c17f13969ab06e714
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-10 19:11:40 +0200

    change function so it just returns the destructured height and width

--- src/utils/resizeProjectImg.js
+++ src/utils/resizeProjectImg.js
@@ -2,11 +2,6 @@ const resizeImg = (card) => {
     const imgSizes = card.fields.projectThumbnail.fields.file.details.image;
     let { height, width } = imgSizes;
 
-    if (height > width) {
-        width = 450;
-        console.log(`${card.fields.projectTitle}`);
-    }
-
     return { height, width };
 };
 
