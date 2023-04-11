ae7e995ee3c9239e8280b1e0c3f389eef2b64153
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-11 20:21:23 +0200

    setup function to extract content from response

---
+++ src/utils/getProjectContent.js
@@ -0,0 +1,56 @@
+const getCardImg = (card) => {
+    const imgSizes = card.fields.projectThumbnail.fields.file.details.image;
+    let { height, width } = imgSizes;
+
+    return { height, width };
+};
+
+const getProjectContent = (res) => {
+    const { title, year, content, slug } = res.fields;
+    console.log(content);
+    getProjectImg(content);
+    getProjectTitle(title);
+    getProjectTxt(content);
+};
+
+// extract images from projects
+const getProjectImg = (content) => {
+    return;
+};
+
+// extracts titles from projects
+
+const getProjectTitle = (title) => {
+    return;
+};
+
+// extract intro texts from projects
+const getProjectTxt = (content) => {
+    return;
+};
+
+// extract videos from projects
+const getProjectVideo = (content) => {
+    return;
+};
+
+// extract pdfs from projects
+const getProjectPdf = (content) => {
+    return;
+};
+
+// extract links from projects
+const getProjectLink = (content) => {
+    return;
+};
+
+export {
+    getCardImg,
+    getProjectImg,
+    getProjectTitle,
+    getProjectTxt,
+    getProjectVideo,
+    getProjectLink,
+    getProjectPdf,
+    getProjectContent,
+};
