c1bce6bf4ae1fcbbd57370b3d41493085eac25d8
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-11 20:22:01 +0200

    rename util function

--- src/utils/resizeProjectImg.js
+++
@@ -1,8 +0,0 @@
-const resizeImg = (card) => {
-    const imgSizes = card.fields.projectThumbnail.fields.file.details.image;
-    let { height, width } = imgSizes;
-
-    return { height, width };
-};
-
-export { resizeImg };
