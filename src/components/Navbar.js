6d694bda15381f33b0843878744ac4e9b64c0d9b
islandskan <sigridureggertsdottir.art@gmail.com>
2023-04-13 15:52:52 +0200

    change link to Works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ export const Navbar = () => {
     };
 
     const links = [
-        { url: '/', name: 'Home' },
+        { url: '/', name: 'Works' },
         { url: '/about', name: 'About' },
         { url: '/news', name: 'News' },
         { url: '/contact', name: 'Contact' },
