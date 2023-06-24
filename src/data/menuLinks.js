5b2fb25174bede66d5c7d428c713abee6ed0f248
islandskan <sigridureggertsdottir.art@gmail.com>
2023-06-24 13:28:07 +0200

    move navbar links to seperate file

---
+++ src/data/menuLinks.js
@@ -0,0 +1,7 @@
+export const links = [
+    { url: '/works', name: 'Works' },
+    { url: '/published', name: 'Published' },
+    { url: '/about', name: 'About' },
+    { url: '/news', name: 'News' },
+    { url: '/contact', name: 'Contact' },
+];
