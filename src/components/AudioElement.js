fbab973b08416414cb75ccbe495d53accceed3ce
islandskan <sigridureggertsdottir.art@gmail.com>
2023-06-26 15:33:09 +0200

    fix audio element

--- src/components/AudioElement.js
+++ src/components/AudioElement.js
@@ -9,23 +9,14 @@ export const AudioElement = ({ audioObj }) => {
     return (
         <div className={styles.audioContainer}>
             <div className={styles.audioInfo}>
-                <h4>{audioImageText}</h4>
+                <audio className={styles.audioPlayer} controls>
+                    <source src={`https:${url}`} type={contentType} />
+                    Your browser does not support the audio tag.
+                </audio>
                 <div className={styles.audioText}>
                     {documentToReactComponents(audioText)}
                 </div>
-                <figure className={styles.audioImgContainer}>
-                    <Image
-                        src={`https:${audioImage.fields.file.url}`}
-                        alt={audioImageText}
-                        height={audioImage.fields.file.details.image.height}
-                        width={audioImage.fields.file.details.image.width}
-                    />
-                </figure>
             </div>
-            <audio className={styles.audioPlayer} controls>
-                <source src={`https:${url}`} type={contentType} />
-                Your browser does not support the audio tag.
-            </audio>
         </div>
     );
 };
