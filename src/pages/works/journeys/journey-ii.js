49489b2015b85edf970bcbfd6caebcaa36c377e0
islandskan <sigridureggertsdottir.art@gmail.com>
2023-06-26 13:42:40 +0200

    work on journey ii

--- src/pages/works/journeys/journey-ii.js
+++ src/pages/works/journeys/journey-ii.js
@@ -2,6 +2,7 @@ import { MetaData } from '../../../components/MetaData.js';
 import { createClient } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { INLINES } from '@contentful/rich-text-types';
+import { ImageCollection } from '../../../components/ImageCollection.js';
 
 import { GoBackLink } from '../../../components/GoBackLink.js';
 
@@ -10,7 +11,7 @@ export async function getStaticProps() {
         space: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     });
-    const res = await client.getEntry('XEoJ3qEqr4YTfXFGvlhQp');
+    const res = await client.getEntry('75DgrpHZeYH2MXOQvUfdzM');
     if (!res) {
         return { notFound: true };
     }
@@ -24,7 +25,10 @@ function Journey2({ res }) {
     const { title, content, slug } = res.fields;
 
     const text = content[0].fields.formattedText;
-    // const journey2Images = content.slice(2);
+
+    const journey2Images = content.slice(1);
+
+    console.log(journey2Images);
 
     const options = {
         renderNode: {
@@ -55,7 +59,7 @@ function Journey2({ res }) {
                     {documentToReactComponents(text, options)}
                 </div>
                 <div className='imageContainer'>
-                    {/* <ImageCollection images={journey2Images} /> */}
+                    <ImageCollection images={journey2Images} />
                 </div>
                 <GoBackLink slug={slug} />
             </div>
