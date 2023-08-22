8e68dbdb3c0eab79392645b09b46678bdfc7752f
islandskan <sigridureggertsdottir.art@gmail.com>
2023-08-22 20:34:32 +0200

    fix responsive video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
+import { RiPlayFill } from 'react-icons/ri';
+import { IconContext } from 'react-icons';
 import styles from '../styles/components/Video.module.css';
 import ReactMarkdown from 'react-markdown';
 
@@ -20,13 +22,26 @@ export const Video = ({ video }) => {
         videoThumbnail.fields.file.details.image;
 
     return (
-        <div className={`video ${styles.videoContainer}`}>
+        // <div className={`video ${styles.videoContainer}`}>
+        <div className='videoContainer'>
             {hasWindow && (
                 <ReactPlayer
                     url={videoUrl}
+                    className={styles.reactPlayer}
                     controls
                     width='100%'
                     height='100%'
+                    playIcon={
+                        <IconContext.Provider
+                            value={{
+                                color: 'white',
+                                size: '20%',
+                                className: 'videoBtn',
+                            }}
+                        >
+                            <RiPlayFill />
+                        </IconContext.Provider>
+                    }
                     light={
                         <Image
                             src={videoThumbnailURL}
