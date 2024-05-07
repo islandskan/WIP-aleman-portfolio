1bf0ba6d2e8a0a92df9fc834f99e74f04403043d
islandskan <sigridureggertsdottir.art@gmail.com>
2024-05-07 18:00:38 +0200

    update image id

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,7 @@ export async function getStaticProps() {
         space: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     });
-    const res = await client.getAsset('3LTpX2P3sknq61VOvaJ2ML');
+    const res = await client.getAsset('4PYIq1YYw2vyO3MPzsj2yK');
     if (!res) {
         return { notFound: true };
     }
